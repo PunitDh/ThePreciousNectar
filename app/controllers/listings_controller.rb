5971e3085bc2b4d13742925a42141c47dbbf8054
PunitDh <cas012115@coderacademy.edu.au>
2021-05-17 21:05:11 +1000

    Update listings index page minor edits to CSS

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,12 +1,12 @@
 class ListingsController < ApplicationController
     before_action :authenticate_user!
+    before_action :listing_find, only: [:show, :edit]
 
     def index
         @listings = Listing.all
     end
 
     def show
-        @listing = Listing.find(params[:id])
         render 'listings/show'
     end
 
@@ -21,14 +21,22 @@ class ListingsController < ApplicationController
         @listing.region_id = params[:listing][:region_id].to_i
         @listing.vintage = params[:listing][:vintage]
         @listing.price = params[:listing][:price].to_i * 100
-        p @listing
-        
         @listing.save
         redirect_to listing_show_path(@listing.id)
+    end
+
+    def edit
+    end
+
+    def update
     end
 
     private
         def listing_params
             params.require(:listing).permit(:name, :vintage, :category_id, :region_id, :description, :price, :image)
+        end
+
+        def listing_find
+            @listing = Listing.find(params[:id])
         end
 end
