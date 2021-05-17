88986e0c0ab2f928b52597f0284a031e3e0d71d1
PunitDh <cas012115@coderacademy.edu.au>
2021-05-17 21:53:53 +1000

    Fix migrations

--- db/migrate/20210517035558_create_listings.rb
+++ db/migrate/20210517035558_create_listings.rb
@@ -6,8 +6,8 @@ class CreateListings < ActiveRecord::Migration[6.1]
       t.text :description
       t.integer :price
       t.integer :vintage
-      # t.references :variety, null: false, foreign_key: true
-      t.references :region, null: false, foreign_key: true
+      # t.integer :category, null: false, foreign_key: true
+      # t.i :region, null: false, foreign_key: true
 
       t.timestamps
     end
