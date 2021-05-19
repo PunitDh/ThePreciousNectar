e4c24843d40762d0d27dc7b38f8fea49a1fa61b9
PunitDh <cas012115@coderacademy.edu.au>
2021-05-20 07:45:17 +1000

    Configure omniauth builder using dig

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV["google_client_id"], ENV["google_client_secret"]
+    provider :google_oauth2, Rails.application.credentials.dig(:google, :google_client_id), Rails.application.credentials.dig(:google, :google_client_secret)
   end
