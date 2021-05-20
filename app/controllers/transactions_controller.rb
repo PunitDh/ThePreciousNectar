5db3270c45277ec3c140314aaa18e15fb7cdf887
PunitDh <cas012115@coderacademy.edu.au>
2021-05-21 03:01:52 +1000

    Configure ENV variables for Stripe

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionsController < ApplicationController
     def success
-        raise params[:format][:id][0].inspect
+        # raise params[:format][:id][0].inspect
         
     end
 
